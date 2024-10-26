0002f401248b08b6efc64e7df0233cf6967caf01
camiloburn <camilosantanadesilva@gmail.com>
2024-10-26 17:04:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import os
 
 app = Flask(__name__)
 
+# Certifique-se de que a pasta downloads existe
+if not os.path.exists("downloads"):
+    os.makedirs("downloads")
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -11,13 +15,16 @@ def index():
         try:
             yt = YouTube(video_url)
             stream = yt.streams.get_highest_resolution()
-            file_path = stream.download()
-            return send_file(file_path, as_attachment=True)
+            file_path = stream.download(output_path="downloads")  # Especifique o caminho de download
+            response = send_file(file_path, as_attachment=True)
+            os.remove(file_path)  # Remove o arquivo após o download
+            return response
         except Exception as e:
             return f"Erro ao baixar o vídeo: {str(e)}"
     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
